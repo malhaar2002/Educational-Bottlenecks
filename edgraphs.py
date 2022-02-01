1a4ba3afd1715d212ebed5322e2ade9e44b528c6
Malhaar Arora <58373047+malhaar2002@users.noreply.github.com>
2022-02-01 15:46:33 +0530

    Add files via upload

--- edgraphs.py
+++ edgraphs.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import csv
 
 #Choose database file - dindori.csv, bhopal.csv or chhatarpur.csv
-database = "csv/dindori.csv"
+database = "csv/chhatarpur.csv"
 
 #Import content from database
 content = []

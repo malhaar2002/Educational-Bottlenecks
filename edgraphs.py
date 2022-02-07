60b3b74e07730c30aae31079b55d8dd218d52591
Malhaar Arora <58373047+malhaar2002@users.noreply.github.com>
2022-02-07 20:47:44 +0530

    Add files via upload

--- edgraphs.py
+++ edgraphs.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import csv
 
 #Choose database file - dindori.csv, bhopal.csv or chhatarpur.csv
-database = "csv/chhatarpur.csv"
+database = "csv/dindori.csv"
 
 #Import content from database
 content = []
@@ -27,8 +27,8 @@ plt.show()
 
 
 #Total Number of Dropouts
-boys_dropout = [total_boys[i]-total_boys[i+1] for i in range(len(total_boys)-1)]
-girls_dropout = [total_girls[i]-total_girls[i+1] for i in range(len(total_girls)-1)]
+boys_dropout = [(total_boys[i]-total_boys[i+1])/total_boys[i+1] for i in range(len(total_boys)-1)]
+girls_dropout = [(total_girls[i]-total_girls[i+1])/total_girls[i+1] for i in range(len(total_girls)-1)]
 plt.plot(classes_for_dropouts, boys_dropout, girls_dropout)
 plt.title("Total Number of Dropouts")
 plt.xlabel("Class")
@@ -37,21 +37,19 @@ plt.legend(["Boys", "Girls"])
 plt.show()
 
 
-#Comparison across General vs ST vs SC vs OBC vs BPL
+#Comparison across General vs ST vs SC vs OBC
 st_total = [int(content[i][2])+int(content[i][3]) for i in range(3, len(content))]
 sc_total = [int(content[i][4])+int(content[i][5]) for i in range(3, len(content))]
 obc_total = [int(content[i][6])+int(content[i][7]) for i in range(3, len(content))]
 general_total = [int(content[i][8])+int(content[i][9]) for i in range(3, len(content))]
-bpl_total = [int(content[i][12])+int(content[i][13]) for i in range(3, len(content))]
 plt.plot(classes, st_total)
 plt.plot(classes, sc_total)
 plt.plot(classes, obc_total)
 plt.plot(classes, general_total)
-plt.plot(classes, bpl_total)
 plt.title("Comparison Across Different Categories")
 plt.xlabel("Class")
 plt.ylabel("Number of Students Enrolled")
-plt.legend(["Scheduled Tribe", "Scheduled Caste", "Other Backward Classes", "General", "Below Poverty Line"])
+plt.legend(["Scheduled Tribe", "Scheduled Caste", "Other Backward Classes", "General"])
 plt.show()
 
 
